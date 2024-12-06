3ad8dd41b7968aecb3be32588ce73b5111013794
marcosfisicaifce <42503833+marcosfisicaifce@users.noreply.github.com>
2024-12-06 07:56:29 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from db import init_db
+
+init_db()
+
+st.set_page_config(page_title="Agendamentos", layout="wide")
+
+st.sidebar.title("Navegação")
+page = st.sidebar.radio("Ir para:", ["Agendar (Professores)", "Administração"])
+
+if page == "Agendar (Professores)":
+    from pages.teacher import app
+    app()
+elif page == "Administração":
+    from pages.admin import app
+    app()
