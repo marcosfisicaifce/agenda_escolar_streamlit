652a96b00907273916178e561555c55a21212cc8
marcosfisicaifce <42503833+marcosfisicaifce@users.noreply.github.com>
2024-12-09 11:13:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import streamlit as st
 from db import init_db
 
+# Inicializa o banco de dados
 init_db()
 
+# Configuração da página
 st.set_page_config(page_title="Agendamentos", layout="wide")
 
-# Navegação principal
+# Barra lateral de navegação
 st.sidebar.title("Navegação")
 page = st.sidebar.radio("Ir para:", ["Agendar (Professores)", "Administração"])
 
+# Navegação entre as páginas
 if page == "Agendar (Professores)":
     from pages.teacher import app
     app()
